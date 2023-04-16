29939dccdb6a9fd6d7521246a6764463de96ded8
Itiel Felix <felix.itiel@uabc.edu.mx>
2023-04-15 20:31:18 -0700

    Finished elements interface

--- app/src/main/java/com/itielfelix/practica4_recyclerview/Element.kt
+++ app/src/main/java/com/itielfelix/practica4_recyclerview/Element.kt
@@ -1,5 +1,40 @@
 package com.itielfelix.practica4_recyclerview
 
-data class Element (val atomicNum:Int, val symbol:String, val name:String,   val atomicMass:String, val oxidationStates:String, val state:String){
+import android.os.Parcel
+import android.os.Parcelable
+
+data class Element(val atomicNum:Int, val symbol: String, val name:String, val atomicMass:String, val oxidationStates:String, val state:String):Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(atomicNum)
+        parcel.writeString(symbol)
+        parcel.writeString(name)
+        parcel.writeString(atomicMass)
+        parcel.writeString(oxidationStates)
+        parcel.writeString(state)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Element> {
+        override fun createFromParcel(parcel: Parcel): Element {
+            return Element(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Element?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
 
--- app/src/main/java/com/itielfelix/practica4_recyclerview/ElementInfo.kt
+++ app/src/main/java/com/itielfelix/practica4_recyclerview/ElementInfo.kt
@@ -1,18 +1,24 @@
 package com.itielfelix.practica4_recyclerview
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.itielfelix.practica4_recyclerview.ui.theme.Practica4_RecyclerViewTheme
 
 class ElementInfo : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,7 +28,8 @@ class ElementInfo : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    val element = intent.getParcelableExtra("element", Element::class.java)
+                    Text(text = element!!.name, fontWeight = FontWeight.Bold)
                 }
             }
         }
